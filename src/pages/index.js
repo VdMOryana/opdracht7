a33789ce71d52c1ab7dffad761bb471a02296491
Oryana Van de Mosselaer <oryana.vandemosselaer@hotmail.com>
2023-04-17 11:14:31 +0200

    Install Swiper, hierop nog verder te werken.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
-import styles from '@/styles/Home.module.css';
-
 import Head from 'next/head';
 import NavigationBar from '@/components/Navigation';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import styles from '@/styles/Home.module.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Home() {
   return (
@@ -9,7 +15,33 @@ export default function Home() {
       <Head>
         <title>Antwerpen Rollers</title>
       </Head>
+
       <main>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide> 
+            <div class="slide-content">
+            <h2>Titel van de slide</h2>
+            <p>Tekst van de slide</p>
+            <button>Knop tekst</button>
+            <br/>
+            <br/>
+            <br/>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide> Slide 2 </SwiperSlide>
+          <SwiperSlide> Slide 3 </SwiperSlide>
+          <SwiperSlide> Slide 4 </SwiperSlide>
+        </Swiper>
+
         <NavigationBar/>
       </main>
     </>
