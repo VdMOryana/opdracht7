38cffcebee7b6db3e6b8cb10e3d3927d082f681e
Oryana Van de Mosselaer <oryana.vandemosselaer@hotmail.com>
2023-05-24 15:14:05 +0200

    Pagina problemen aangevuld

--- src/pages/problemen.js
+++ src/pages/problemen.js
@@ -20,7 +20,25 @@ export default function Problemen() {
               alt="Logo"
               />
           </div>
+
+          <div>
+            <h1 className={styles.TussenTitels}> Problemen bij nemen of terugplaatsen fiets </h1>
+            <p className={styles.OnderTitels}> Slotnummer </p>
+            <input className={styles.SearchBalk} type="text"/>
+
+            <p className={styles.OnderTitels}> Fietsnummer </p>
+            <input className={styles.SearchBalk} type="text"/>
+
+            <p className={styles.OnderTitels}> Stationsnummer </p>
+            <input className={styles.SearchBalk} type="text"/>
+
+          </div>
+
+
+          <button className={styles.SearchButton} > Verzenden </button>
+
         </main>
         </>
     )
 }
+
